4c90a6f70a8ce366dbb197f2cb38bcec5e01fb71
developer.niroj <developer.niroj@gmail.com>
2021-07-26 18:36:30 +0545

    converters added

---
+++ Converters/IsValueMatchedConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Xam.Helpers.Converters
+{
+    public class IsValueMatchedConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2)
+                return false;
+            if (values[0] == null || values[1] == null)
+                return true;
+            return values[0].ToString() == values[1].ToString();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
